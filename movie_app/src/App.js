ed02f6c7741564548d827bfc55d6f29a24345dd4
sanha <kgpaper@naver.com>
2021-03-17 04:12:03 +0900

    21/03/17 studying

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,21 +1,43 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
-  getMovies = async () =>{
-    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
-  }
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    // const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json"); 와 같은 의미의 ES6 문법
+    this.setState({ movies, isLoading: false });
+  };
 
   componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie) => (
+              <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
